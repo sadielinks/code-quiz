67df732675ee1e0d52af18a2fb61be50ede93102
Sadie Sial <sadiesial@gmail.com>
2021-08-20 19:22:16 -0500

    started messing with the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,60 +6,82 @@ var quizStartBtn = document.querySelector("#startBtn");
 var quizScoreBtn = document.querySelector("#submitBtn");
 var timeTimeBaby = document.querySelector("#timetimebaby");
 var finalScoresHere = document.querySelector("#quizhighscore");
-var initialsGoHere =document.querySelector("#intialsgohere");
+var initialsGoHere = document.querySelector("#intialsgohere");
 
 var time = 100
-var timerId
+var timerInterval
 
 var currentQuestionIndex = 0;
 
+// Quiz questions + their respective answers
 var question = [
     {
         questionName: "This is a question here?",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 2",
+    },
+    {
+        questionName: "This is a question here? 2",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 3",
+    },
+    {
+        questionName: "This is a question here? 3",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 4",
+    },
+    {
+        questionName: "This is a question here? 4",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 5",
+    },
+    {
+        questionName: "This is a question here? 5",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 6",
+    },
+    {
+        questionName: "This is a question here? 6",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 7",
+    },
+    {
+        questionName: "This is a question here? 7",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 8",
+    },
+    {
+        questionName: "This is a question here? 8",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 9",
+    },
+    {
+        questionName: "This is a question here? 9",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
-    }, 
-        {questionName: "This is a question here? 10",
+    },
+    {
+        questionName: "This is a question here? 10",
         choices: ["answer1", "answer2", "answer3", "answer4"],
         answer: "answer1"
     }
 ]
 
-// timer code goes here eventually
+function timeTimeBaby() {
 
-function startQuiz (){
+var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerInterval.textContent = secondsLeft + " seconds";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      quizScoreHere();
+    }
+  }, 1000);
+// quizStartHere.style.display = "block";
+}
+
+function startQuiz() {
     // start timer, hide start screen, unhide questions, call next function to get questions
     quizStartHere.setAttribute("class", "hide")
     quizQuestionsHere.removeAttribute("class", "hide")
